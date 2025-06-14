12ee10d96b7c6c3689cb27b6112f2d69cbf2647c
Jose Allain <joseallain27@gmail.com>
2025-06-14 13:19:24 -0500

    HU-10 completa

--- app/src/main/java/com/example/prodent/view/MapaActivity.kt
+++ app/src/main/java/com/example/prodent/view/MapaActivity.kt
@@ -2,12 +2,15 @@ package com.example.prodent.view
 
 import android.content.Intent
 import android.graphics.Paint
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.prodent.R
 import com.example.prodent.databinding.ActivityMapaBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import androidx.core.net.toUri
 
 class MapaActivity : AppCompatActivity() {
 
@@ -19,16 +22,13 @@ class MapaActivity : AppCompatActivity() {
         binding = ActivityMapaBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Subrayar texto
         binding.verhorariosTv.paintFlags = binding.verhorariosTv.paintFlags or Paint.UNDERLINE_TEXT_FLAG
 
-        // Obtener rol del usuario
         val uid = FirebaseAuth.getInstance().currentUser?.uid ?: return
         FirebaseFirestore.getInstance().collection("usuarios").document(uid).get()
             .addOnSuccessListener { document ->
                 rolUsuario = document.getString("rol") ?: ""
 
-                // Configurar navegación inferior después de conocer el rol
                 binding.bottomNavigationView.setOnItemSelectedListener { item ->
                     when (item.itemId) {
                         R.id.nav_home -> {
@@ -57,6 +57,34 @@ class MapaActivity : AppCompatActivity() {
                         }
 
                         else -> false
+                    }
+                }
+                binding.verhorariosTv.setOnClickListener {
+                    val dialogView = layoutInflater.inflate(R.layout.dialog_horarios, null)
+                    AlertDialog.Builder(this)
+                        .setTitle("Horarios de Atención")
+                        .setView(dialogView)
+                        .setPositiveButton("Cerrar") { dialog, _ ->
+                            dialog.dismiss()
+                        }
+                        .show()
+                }
+
+                binding.apply {
+                    whatsIv.setOnClickListener {
+                        val url = "https://web.whatsapp.com/send?phone=+51987529718&text=Hola"
+                        val intent = Intent(Intent.ACTION_VIEW, url.toUri())
+                        startActivity(intent)
+                    }
+                    instaIv.setOnClickListener {
+                        val url = "https://www.instagram.com/prodentperu/"
+                        val intent = Intent(Intent.ACTION_VIEW, url.toUri())
+                        startActivity(intent)
+                    }
+                    faceIv.setOnClickListener {
+                        val url = "https://www.facebook.com/prodent.pe/?locale=es_LA"
+                        val intent = Intent(Intent.ACTION_VIEW, url.toUri())
+                        startActivity(intent)
                     }
                 }
             }
